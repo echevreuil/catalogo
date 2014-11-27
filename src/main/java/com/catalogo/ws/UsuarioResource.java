d81f28bbdeca80f32ceff028cda7a2f114d65f15
wellington <wchevreuil@cloudera.com>
2014-11-27 23:01:23 +0000

    rest

--- src/main/java/com/catalogo/ws/UsuarioResource.java
+++ src/main/java/com/catalogo/ws/UsuarioResource.java
@@ -1,5 +1,6 @@
 package com.catalogo.ws;
 
+import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -7,16 +8,23 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import com.catalogo.domain.Usuario;
+import com.catalogo.service.UsuarioService;
+
 @Path("usuarios")
 public class UsuarioResource {
 
+  @Inject
+  private UsuarioService service;
 
   @POST
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.APPLICATION_JSON)
-  public String insere(String nome, String email) {
+  public Response insere(Usuario usuario) {
 
-    return Response.status(201).entity(result).build();
+    usuario = service.criarUsuario(usuario);
+
+    return Response.status(201).entity(usuario).build();
 
   }
 
